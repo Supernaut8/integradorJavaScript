603abb52e39faad8a9e396d6dab366836eb59c53
Guido <gnroces@gmail.com>
2023-05-31 18:04:34 -0300

    integradorJavaScript

---
+++ tickets.js
@@ -0,0 +1,50 @@
+const valor = 200;
+const cantidad = document.getElementById("cantidad");
+const categoria = document.getElementById("categoria");
+const btnResumen = document.getElementById("resumen");
+const total = document.getElementById("total")
+const btnBorrar = document.getElementById("borrar")
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const correo = document.getElementById("correo")
+
+btnResumen.addEventListener("click", calculaTotal);
+btnBorrar.addEventListener("click", limpiaCampos);
+
+
+function descuento(cat) {
+    let ticket;
+    
+    if (categoria.value == 1) {
+        dto = 0.8;
+        ticket = valor - (valor * dto);
+    }    
+    else    
+        if (categoria.value == 2) {
+            dto = 0.5;
+            ticket = valor - (valor * dto);
+        }    
+    else {
+        dto = 0.15;
+        ticket = valor - (valor * dto);   
+    }
+    return ticket;         
+}
+
+/*addEventListener(click, calculaCosto) getElementById("resumen");
+calculaCosto(valor, cantidad);*/
+
+
+function calculaTotal() {    
+    
+    total.innerHTML = console.log("Total a pagar: $");
+    total.innerHTML = cantidad.value * descuento(categoria.value) ;
+}
+
+function limpiaCampos() {
+    nombre.value = "";
+    apellido.value = "";
+    correo.value = "";
+    cantidad.value = "";
+    total.innerHTML = "";
+}
